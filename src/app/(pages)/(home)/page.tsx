cd852117d4b9e7baa5c9f1506aeac719dfb8c2c9
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-09-21 15:59:32 -0300

    feat: add layout of the skills section

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,11 +1,13 @@
 import About from "./components/about/About";
 import Hero from "./components/hero/Hero";
+import Skills from "./components/skills/Skills";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <About />
+      <Skills />
     </>
   );
 }
