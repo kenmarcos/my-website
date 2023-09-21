ac02d8e12725f81fcec8e87577104866d6f47073
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-09-21 09:40:36 -0300

    fix: save layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={openSans.className}>
-<<<<<<< HEAD
-        <div className="container relative md:flex">
-=======
         <div className="max-w-[1440px] mx-auto relative md:flex">
->>>>>>> 6b2479a3694f5827e837f4eda1c2a835c96d61e8
           <Sidebar />
 
           <main className="flex-1">{children}</main>
