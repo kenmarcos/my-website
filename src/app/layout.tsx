38e2d76a23ca93c8eddf081633959570758cb8ec
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-09-23 20:13:26 -0300

    style: add scroll-smooth to html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className="scroll-smooth">
       <body className={openSans.className}>
         <div className="max-w-[1440px] mx-auto relative md:flex">
           <Sidebar />
