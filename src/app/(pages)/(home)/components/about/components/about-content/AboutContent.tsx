5fa3853f2a475eb288b8190f7cb794845a48cb50
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-09-26 16:31:51 -0300

    feat: add blur data url to my profile image

--- src/app/(pages)/(home)/components/about/components/about-content/AboutContent.tsx
+++ src/app/(pages)/(home)/components/about/components/about-content/AboutContent.tsx
@@ -3,13 +3,18 @@ import Image from "next/image";
 const AboutContent = () => {
   return (
     <section className="mt-12 flex flex-wrap gap-12 justify-center">
-      <Image
-        className="rounded-md -scale-x-100 object-cover aspect-[3/4]"
-        src="https://github.com/kenmarcos.png"
-        alt="Marcos"
-        width={220}
-        height={220}
-      />
+      <div className="relative h-80 aspect-[3/4]">
+        <Image
+          className="rounded-md -scale-x-100 object-cover"
+          src="https://github.com/kenmarcos.png"
+          alt="Profile image"
+          fill
+          sizes="100%"
+          quality={100}
+          placeholder="blur"
+          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMMvAYAAX4BKd5Md4EAAAAASUVORK5CYII="
+        />
+      </div>
 
       <div className="flex flex-col gap-4 max-w-xl justify-center">
         <p>
