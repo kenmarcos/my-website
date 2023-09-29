fcdc36df1728441479cab187870b9a9520783cc6
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-09-29 18:03:05 -0300

    feat: change image in about dection

--- src/app/(pages)/(home)/components/about/components/about-content/AboutContent.tsx
+++ src/app/(pages)/(home)/components/about/components/about-content/AboutContent.tsx
@@ -2,24 +2,33 @@ import Image from "next/image";
 
 import { gql } from "@apollo/client";
 import { getClient } from "lib/client";
+import { About } from "types";
 
 const AboutContent = async () => {
-  const { data } = await getClient().query({
+  const { data } = await getClient().query<{ about: About }>({
     query: gql`
       query GetAbout {
         about(where: { id: "cln4y2kmhbgbg0blx46ffbmyq" }) {
           aboutText
+          image {
+            url
+          }
         }
       }
     `,
+    context: {
+      fetchOptions: {
+        next: { revalidate: 60 * 60 * 24 * 15 }, // revalidate every 15 days
+      },
+    },
   });
 
   return (
     <section className="mt-12 flex flex-wrap gap-12 justify-center">
       <div className="relative h-80 aspect-[3/4]">
         <Image
-          className="rounded-md -scale-x-100 object-cover"
-          src="https://github.com/kenmarcos.png"
+          className="rounded-md object-cover"
+          src={data.about.image.url}
           alt="Profile image"
           fill
           sizes="100%"
