7223dfd4fb71f36d08a5dc8939dcfbe5ffb8f54e
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-09-29 15:25:31 -0300

    fix: change text color to inputs

--- src/app/(pages)/contacts/components/message-submission/MessageSubmission.tsx
+++ src/app/(pages)/contacts/components/message-submission/MessageSubmission.tsx
@@ -6,21 +6,21 @@ const MessageSubmission = () => {
   return (
     <section className="lg-order-last lg:col-span-2">
       <form className="space-y-6">
-        <div className="flex flex-wrap gap-6">
+        <div className="flex flex-wrap gap-6 text-gray-primary">
           <Input placeholder="Seu nome" />
 
           <Input placeholder="Seu e-mail" />
         </div>
 
-        <div>
+        <div className="text-gray-primary">
           <Input placeholder="Assunto" />
         </div>
 
-        <div>
+        <div className="text-gray-primary">
           <Textarea placeholder="Mensagem" rows={7} />
         </div>
 
-        <div className="text-center">
+        <div className="text-end">
           <Button className="bg-blue-primary w-full md:max-w-[160px]">
             Enviar Menssagem
           </Button>
